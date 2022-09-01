907e501ea3e458ff2f301c2ff4bd0bc1c516574f
kepler24 <53371708+kepler24@users.noreply.github.com>
2022-09-01 10:37:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -25,10 +25,7 @@ export const config = {
     //   id: "wxafasdasdd-zxfvsdfd"
     // }]
     USERS: [
-      {name: '杰伦的燕姿', id: "o18wE55OJsnDvJU1Sr3VQgcFihWs"}, 
-      {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
-      {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-      {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
+      {name: '杰伦的燕姿', id: "o18wE55OJsnDvJU1Sr3VQgcFihWs"},
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
